f8c3968cea85180dfa0990f664861aebb624c103
Jesehu <github@jessca.name>
2024-11-23 23:28:03 -0700

    added recipes

---
+++ recipes/asian salad.lisp
@@ -0,0 +1,34 @@
+(recipe "asian salad" (
+	(description "")
+	(ingredients [
+		(salad [
+			(cucumber)
+			(shredded carrot/daikon mix)
+			(shredded papaya)
+			(cabbage)
+			(green onion)
+			(cilantro)
+			(basil)
+			])
+		(dressing [
+			(rice vinegar)
+			(garlic)
+			(sriracha)
+			(soy sauce)
+			(fish sauce)
+			(sugar)
+			(sesame oil)
+			(limes)
+			])
+		])
+	(instructions 
+		"mince garlic, combine all dressing ingredients."
+		"chop salad ingredients, combine with dressing"
+		"serve over rice noodles"
+		)
+	)
+)
+		
+		
+			
+		
---
+++ recipes/simple spanish rice.lisp
@@ -0,0 +1,21 @@
+(recipe "simple spanish rice" (
+	(description "")
+	(ingredients [
+		((1 cup) brown basmati rice)
+		((1.5 cups) chicken broth)
+		((1 cup) salsa or canned tomatoes)
+		((1 can) black beans)
+		((1 can) corn)
+		((1 tbsp) chimichuri spice)
+		((1 tsp) garlic powder)
+		((1 tsp) cumin)
+		((1 tsp) chili powder)
+		])
+	(instructions [
+		"dry-roast rice with spices in skillet until rice is lightly browned"
+		"heat 1 tbsp oil and transfer rice to saute pan"
+		"add chicken broth, simmer for 2 minutes"
+		"add salsa, tomatoes, beans, corn"
+		"cover and simmer until rice is done, adding more water if needed"
+		])
+	))
